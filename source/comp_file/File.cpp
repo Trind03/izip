b1370fdfd04563db5ee7df7731c377846f0a8551
Torstein <torstein@Torsteins-MacBook-Pro.local>
2025-04-13 13:04:11 +0200

    Remove archive type enum. Add function for getting file size.

--- source/comp_file/File.cpp
+++ source/comp_file/File.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include "File.h"
 #include <string>
+#include <fstream>
 #include "exit_codes.hpp"
 #include <spdlog/spdlog.h>
 #include <archive.h>
@@ -21,34 +22,20 @@ namespace wrappers
             return "";
         }
 
-
-        int File::extract_tarball(std::string filename,std::string output_dir)
-        {
-            struct archive_entry* entry;
-            struct archive* input_file;
-            struct archive* output_file;
-            input_file = archive_read_new();
-
-            archive_read_support_filter_all(input_file);
-            archive_read_support_format_all(input_file);
-
-            if ((archive_read_open_filename(input_file,filename.c_str(),1240)))
-            {
-                spdlog::error("Error occured during extraction, did u effect the file?");
-                return EXIT_CODE::FAILURE;
-            }
-
-            return EXIT_CODE::SUCCESS;
-        }
-
         int File::get_exit_code()
         {
             return 0;
         }
 
-        int File::read_file()
+        void File::read_file()
         {
-            return EXIT_CODE::SUCCESS;
+            return;
+        }
+
+        std::ifstream::pos_type File::get_filesize(const char* filename)
+        {
+            std::ifstream stream(filename,std::ifstream::ate | std::ifstream::binary);
+            return stream.tellg();
         }
     }
 }
