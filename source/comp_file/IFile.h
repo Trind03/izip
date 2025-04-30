d0c1d259f4fc72f5022a6809f6a318387a5c1805
Torstein <torsrind@outlook.com>
2025-04-30 22:17:26 +0200

    Did something..

--- source/comp_file/IFile.h
+++ source/comp_file/IFile.h
@@ -11,7 +11,7 @@ namespace wrappers
         {
         public:
             virtual archive* read_load_archive(const char* filename)            = 0;
-            virtual int write_file_to_disk(struct archive* myarchive)           = 0;
+            virtual int write_file_to_disk(struct archive* myarchive,struct archive_entry* entry)           = 0;
             virtual archive_entry* render_archive_entry()                       = 0;
             virtual  ~IFile()                                                   = default;
             virtual int get_exit_code()                                         = 0;
