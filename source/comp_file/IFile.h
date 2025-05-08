eb87d294ecc126f5d751ac7a18c4be6bbf876fa3
Torstein <torsrind@outlook.com>
2025-05-08 21:52:44 +0200

    Create recursive_processing file, for processing recursively like archive folders. Add recursive_decompression function to previously mentioned file. Add logic for checking if its an directory passed to the software. Add recursive_processing.cpp to init.cmake for linking to target.

--- source/comp_file/IFile.h
+++ source/comp_file/IFile.h
@@ -10,7 +10,7 @@ namespace wrappers
         {
         public:
             virtual int decompress_archive(const char* filename)            = 0;
-
+            virtual int recursive_decompression(struct archive *Archive, struct archive_entry *Archive_entry, const char* prefix) = 0;
             virtual archive_entry* render_archive_entry()                       = 0;
             virtual  ~IFile()                                                   = default;
             virtual int get_exit_code()                                         = 0;
